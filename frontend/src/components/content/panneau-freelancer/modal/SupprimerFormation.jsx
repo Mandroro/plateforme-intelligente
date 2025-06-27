fd38add53af8291d3730766aeb5f92226bfcc7d7
mandroro <yvesaimable42@gmail.com>
2025-06-27 14:38:56 +0300

    Ajout du modal de confirmation pour supprimer un competence et formation

---
+++ frontend/src/components/content/panneau-freelancer/modal/SupprimerFormation.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import axios from "axios";
+import { useApiConfig } from "../../../../ApiUrlConfiguration";
+
+export default function SupprimerFormation({ id, open, setOpen, handleClose }) {
+  const { ApiURL } = useApiConfig();
+  const token = localStorage.getItem("token");
+
+  const supprimer = () => {
+    axios
+      .delete(`${ApiURL}/formations/${id}`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Message: ", response.data.message);
+          setOpen(false);
+        }
+      })
+      .catch((error) => {
+        console.log("Erreur inattendue: ", error);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{ fontFamily: "Sora", fontSize: 18 }}
+        >
+          {"Confirmation"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            sx={{ fontFamily: "Sora", fontSize: 14 }}
+          >
+            Voulez vous vraiment supprimer cette formation ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            sx={{ fontFamily: "Sora", fontSize: 14, textTransform: "inherit" }}
+          >
+            Non
+          </Button>
+          <Button
+            onClick={supprimer}
+            sx={{ fontFamily: "Sora", fontSize: 14, textTransform: "inherit" }}
+          >
+            Oui
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
