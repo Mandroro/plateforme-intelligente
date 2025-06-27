687ccc7e317fb49dc06338e247e97c936a7bf887
mandroro <yvesaimable42@gmail.com>
2025-06-27 14:40:12 +0300

    Ajout de nouveau modification sur le composant pour afficher les informations sur un freelancer

--- frontend/src/components/content/panneau-freelancer/CompteFreelancer.jsx
+++ frontend/src/components/content/panneau-freelancer/CompteFreelancer.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import Profil1 from "./../../images/exemple-profil-candidat/1.png";
-import { PenBox, Plus, PlusSquare, XCircle } from "lucide-react";
+import { PenBox, Plus, XCircle } from "lucide-react";
 import { useApiConfig } from "../../../ApiUrlConfiguration";
 import axios from "axios";
 import NouveauFormation from "./modal/NouveauFormation";
 import NouveauCompetence from "./modal/NouveauCompetence";
+import InformationProfil from "./modal/InformationProfil";
+import SupprimerFormation from "./modal/SupprimerFormation";
+import SupprimerCompetence from "./modal/SupprimerCompetence";
 
 export default function CompteFreelancer() {
   const { ApiURL } = useApiConfig();
@@ -15,10 +18,15 @@ export default function CompteFreelancer() {
   const [poste, setPoste] = useState("");
   const [adresse, setAdresse] = useState("");
   const [telephone, setTelephone] = useState("");
+  const [open, setOpen] = useState(false);
   const [openFormation, setOpenFormation] = useState(false);
   const [openCompetence, setOpenCompetence] = useState(false);
   const [dataFormation, setDataFormation] = useState([]);
   const [dataCompetence, setDataCompetence] = useState([]);
+  const [idFormation, setIdFormation] = useState("");
+  const [idCompetence, setIdCompetence] = useState("");
+  const [openSupprimerFormation, setOpenSupprimerFormation] = useState(false);
+  const [openSupprimerCompetence, setOpenSupprimerCompetence] = useState(false);
 
   useEffect(() => {
     axios
@@ -46,9 +54,9 @@ export default function CompteFreelancer() {
         },
       })
       .then((response) => {
-        setPoste(response.data.resultat.freelancer.poste_actuel);
+        setPoste(response.data.resultat.freelancer.poste_travail);
         setAdresse(response.data.resultat.freelancer.adresse_actuel);
-        setTelephone(response.data.resultat.freelancer.numero_telephone);
+        setTelephone(response.data.resultat.freelancer.num_telephone);
         setDataFormation(response.data.resultat.freelancer.formations);
         setDataCompetence(response.data.resultat.freelancer.competences);
       })
@@ -57,7 +65,15 @@ export default function CompteFreelancer() {
       });
   };
 
-  // Gérer formation
+  // Gérer profil
+  const ouvrirInformationProfil = () => {
+    setOpen(true);
+  };
+  const fermerInformationProfil = () => {
+    setOpen(false);
+  };
+
+  // Gérer formation (Ajout - Suppression)
   const ajouterFormation = () => {
     setOpenFormation(true);
   };
@@ -65,12 +81,28 @@ export default function CompteFreelancer() {
     setOpenFormation(false);
   };
 
-  // Gérer compétence
+  const supprimerFormation = (id) => {
+    setIdFormation(id);
+    setOpenSupprimerFormation(true);
+  };
+  const fermerSupprimerFormation = () => {
+    setOpenSupprimerFormation(false);
+  };
+
+  // Gérer compétence (Ajout - Suppression)
   const ajouterCompetence = () => {
     setOpenCompetence(true);
   };
   const fermerCompetence = () => {
     setOpenCompetence(false);
+  };
+
+  const supprimerCompetence = (id) => {
+    setIdCompetence(id);
+    setOpenSupprimerCompetence(true);
+  };
+  const fermerSupprimerCompetence = () => {
+    setOpenSupprimerCompetence(false);
   };
 
   return (
@@ -108,7 +140,10 @@ export default function CompteFreelancer() {
             <h1 className="text-white font-[Sora] text-[16px]">
               Information sur mon profil
             </h1>
-            <button className="p-2 text-white hover:text-gray-500 rounded-md cursor-pointer">
+            <button
+              onClick={ouvrirInformationProfil}
+              className="p-2 text-white hover:text-gray-500 rounded-md cursor-pointer"
+            >
               <PenBox />
             </button>
           </div>
@@ -150,7 +185,7 @@ export default function CompteFreelancer() {
               onClick={ajouterFormation}
               className="p-2 text-white font-[Sora] text-[16px] hover:text-gray-500 flex items-center justify-center rounded-md cursor-pointer"
             >
-              <Plus className="mr-1"/>
+              <Plus className="mr-1" />
               Ajouter
             </button>
           </div>
@@ -169,8 +204,11 @@ export default function CompteFreelancer() {
                       {d.etablissement} - {d.lieu_formation}
                     </p>
                   </div>
-                  <button className="text-white hover:text-gray-500 rounded-md cursor-pointer">
-                    <XCircle/>
+                  <button
+                    onClick={() => supprimerFormation(d.id)}
+                    className="text-white hover:text-gray-500 rounded-md cursor-pointer"
+                  >
+                    <XCircle />
                   </button>
                 </div>
               ))
@@ -192,32 +230,33 @@ export default function CompteFreelancer() {
               onClick={ajouterCompetence}
               className="p-2 text-white font-[Sora] text-[16px] hover:text-gray-500 flex items-center justify-center rounded-md cursor-pointer"
             >
-              <Plus className="mr-1"/>
+              <Plus className="mr-1" />
               Ajouter
             </button>
           </div>
-          <div className="grid grid-cols-6 gap-2 mb-2">
-            {dataCompetence.length > 0 ? (
-              dataCompetence.map((d) => (
-                <div
+
+          {dataCompetence.length > 0 ? (
+            <div className="mt-3 flex flex-wrap gap-2">
+              {dataCompetence.map((d) => (
+                <span
                   key={d.id}
-                  className="col-start-1 col-end-7 bg-gray-800 rounded-md p-3"
+                  className="flex items-center justify-between bg-gray-800 text-white text-[14px] font-[Sora] p-3 rounded-full"
                 >
-                  <input
-                    className="w-full border-gray-200 text-gray-500  font-[Sora] focus:outline-none"
-                    value={d.description}
-                    readOnly
+                  {d.description}
+                  <XCircle
+                    onClick={() => supprimerCompetence(d.id)}
+                    className="ml-4 hover:text-gray-500 cursor-pointer"
                   />
-                </div>
-              ))
-            ) : (
-              <div className="text-center col-start-1 col-end-7 bg-gray-800 rounded-md p-3">
-                <p className="font-[Sora] text-white">
-                  Aucune compétencer enregistrer
-                </p>
-              </div>
-            )}
-          </div>
+                </span>
+              ))}
+            </div>
+          ) : (
+            <div className="text-center col-start-1 col-end-7 bg-gray-800 rounded-md p-3">
+              <p className="font-[Sora] text-white">
+                Aucune compétencer enregistrer
+              </p>
+            </div>
+          )}
         </div>
 
         {/* Gérer le mot de passe */}
@@ -251,6 +290,14 @@ export default function CompteFreelancer() {
         </div>
       </div>
 
+      {/* Formulaire information profil */}
+      <InformationProfil
+        id={id}
+        open={open}
+        setOpen={setOpen}
+        fermerInformationProfil={fermerInformationProfil}
+      />
+
       {/* Formulaire ajout formation */}
       <NouveauFormation
         id={id}
@@ -259,8 +306,29 @@ export default function CompteFreelancer() {
         fermerFormation={fermerFormation}
       />
 
+      {/* Supprimer un formation */}
+      <SupprimerFormation
+        id={idFormation}
+        open={openSupprimerFormation}
+        setOpen={setOpenSupprimerFormation}
+        handleClose={fermerSupprimerFormation}
+      />
+
       {/* Formulaire ajout compétence */}
-      <NouveauCompetence id={id} open={openCompetence} setOpen={setOpenCompetence} fermerCompetence={fermerCompetence}/>
+      <NouveauCompetence
+        id={id}
+        open={openCompetence}
+        setOpen={setOpenCompetence}
+        fermerCompetence={fermerCompetence}
+      />
+
+      {/* Supprimer un compétence */}
+      <SupprimerCompetence
+        id={idCompetence}
+        open={openSupprimerCompetence}
+        setOpen={setOpenSupprimerCompetence}
+        handleClose={fermerSupprimerCompetence}
+      />
     </>
   );
 }
