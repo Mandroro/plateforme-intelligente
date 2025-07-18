2f4e3e88cf9b51d373b92b2ef46130829c4c9a97
mandroro <yvesaimable42@gmail.com>
2025-07-18 14:13:45 +0300

    Modification du composant pour afficher les détails sur un candidat

--- frontend/src/components/content/site-vitrine/content/DetailCandidat.jsx
+++ frontend/src/components/content/site-vitrine/content/DetailCandidat.jsx
@@ -1,4 +1,3 @@
-import { useLocation } from "react-router";
 import { useApiConfig } from "../../../../ApiUrlConfiguration";
 import { Button } from "@mui/material";
 import {
@@ -8,28 +7,42 @@ import {
   Check,
   CheckCircle2Icon,
   ChevronLeft,
-  Circle,
+  Mail,
   MapPin,
+  Phone,
 } from "lucide-react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
-export default function DetailOffre() {
+export default function DetailCandidat() {
   const { ApiURL } = useApiConfig();
-  const id = localStorage.getItem("OFFRE_ID_DETAIL");
+  const redirection = useNavigate();
+  const id = localStorage.getItem("CANDIDAT_ID_DETAIL");
   const [data, setData] = useState([]);
+  const [dataFreelancer, setDataFreelancer] = useState([]);
+  const [dataFormation, setDataFormation] = useState([]);
+  const [dataCompetence, setDataCompetence] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`${ApiURL}/liste-des-offres/${id}`)
+      .get(`${ApiURL}/liste-des-candidats/${id}`)
       .then((response) => {
         console.log(response.data.resultat);
         setData(response.data.resultat);
+        setDataFreelancer(response.data.resultat.freelancer);
+        setDataFormation(response.data.resultat.freelancer.formations);
+        setDataCompetence(response.data.resultat.freelancer.competences);
       })
       .catch((error) => {
         console.log("Erreur inattendue:", error);
       });
   }, [id]);
+
+  const retourCandidats = () => {
+    localStorage.removeItem("CANDIDAT_ID_DETAIL");
+    redirection("/candidats");
+  };
 
   return (
     <>
@@ -39,23 +52,24 @@ export default function DetailOffre() {
             Détails sur le candidats :
           </h1>
           <h1 className="text-white font-[Sora] font-bold text-[40px] uppercase">
-            {data.titre_offre}
+            {data.name}
           </h1>
           <ul className="text-white font-[Sora] font-extralight flex items-center justify-center space-x-10 mb-8">
             <li className="flex items-center">
-              <Building className="mr-2" />
-              Recruteur : Hi-tech
+              <BriefcaseBusiness className="mr-2" />
+              Poste : {dataFreelancer.poste_travail}
             </li>
             <li className="flex items-center">
               <MapPin className="mr-2" />
-              Lieu : Paris, France
+              Lieu : {dataFreelancer.adresse_actuel}
             </li>
             <li className="flex items-center">
-              <CalendarDays className="mr-2" />
-              Publié le : 19 Juin 2025
+              <Mail className="mr-2" />
+              Email : {data.email}
             </li>
           </ul>
           <Button
+            onClick={retourCandidats}
             variant="contained"
             sx={{
               color: "white",
@@ -65,7 +79,7 @@ export default function DetailOffre() {
             }}
             startIcon={<ChevronLeft />}
           >
-            Retour vers les offres
+            Retour vers la liste des candidats
           </Button>
         </div>
       </div>
@@ -74,50 +88,41 @@ export default function DetailOffre() {
           <div className="grid grid-cols-6 gap-8 mb-8">
             <div className="col-start-2 col-end-6">
               <div className="p-4 rounded-md mb-3">
-                <h1 className="text-white font-[Sora] font-bold">
-                  Description
-                </h1>
-                <p className="text-gray-400 font-[Sora] font-light">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Placeat, aut minus nisi voluptates in esse error fugit,
-                  officia repellat quo obcaecati animi quam suscipit fugiat nemo
-                  corporis quibusdam impedit libero.
-                </p>
-              </div>
-              <div className="p-4 rounded-md mb-3">
-                <h1 className="text-white font-[Sora] font-bold">Mission</h1>
+                <h1 className="text-white font-[Sora] font-bold">Formation</h1>
                 <ul className="mt-3">
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <CheckCircle2Icon className="mr-2" />
-                    Mission 1
-                  </li>
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <CheckCircle2Icon className="mr-2" />
-                    Mission 1
-                  </li>
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <CheckCircle2Icon className="mr-2" />
-                    Mission 1
-                  </li>
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <CheckCircle2Icon className="mr-2" />
-                    Mission 1
-                  </li>
+                  {dataFormation.length > 0 ? (
+                    dataFormation.map((d) => (
+                      <li className="text-gray-400 font-[Sora] font-extralight mb-3">
+                        <h1 className="text-white  font-[Sora]">
+                          {d.titre_formation} - {d.annee_formation}
+                        </h1>
+                        <p className="text-gray-500  font-[Sora]">
+                          {d.etablissement} - {d.lieu_formation}
+                        </p>
+                      </li>
+                    ))
+                  ) : (
+                    <p className="text-gray-400 font-[Sora] font-extralight">
+                      Aucun données disponible
+                    </p>
+                  )}
                 </ul>
               </div>
               <div className="p-4 rounded-md">
-                <h1 className="text-white font-[Sora] font-bold">
-                  Compétence requis
-                </h1>
+                <h1 className="text-white font-[Sora] font-bold">Compétence</h1>
                 <ul className="mt-3">
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <Check className="mr-2" />
-                    Mission 1
-                  </li>
-                  <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
-                    <Check className="mr-2" />
-                    Mission 1
-                  </li>
+                  {dataCompetence.length > 0 ? (
+                    dataCompetence.map((d, index) => (
+                      <li className="text-gray-400 font-[Sora] font-extralight flex items-center mb-3">
+                        <Check className="mr-2" />
+                        {d.description}
+                      </li>
+                    ))
+                  ) : (
+                    <p className="text-gray-400 font-[Sora] font-extralight">
+                      Aucun données disponible
+                    </p>
+                  )}
                 </ul>
               </div>
             </div>
